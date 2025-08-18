ce0271377940a62717e4d330e9650d4e3a4233d7
Ray Kofoed <rayhadio62@gmail.com>
2025-08-18 00:11:13 -0500

    SVCroutes

--- 8.14.25_Rest_API_Design_Patterns/app/blueprints/service_tickets/routes.py
+++ 8.14.25_Rest_API_Design_Patterns/app/blueprints/service_tickets/routes.py
@@ -1,5 +1,5 @@
-from app.blueprints.service_tickets import services_tickets_bp 
-from .schemas import services_ticket_schema, services_tickets_schema
+from app.blueprints.service_tickets import service_tickets_bp 
+from .schemas import services_ticket_schema, service_tickets_schema
 from flask import request, jsonify
 from marshmallow import VaidationError
 from app.models import Service_Tickets, db
@@ -17,11 +17,11 @@ def create_service_ticket():
     new_service_ticket = Service_Tickets(**data)
     db.session.add(new_service_ticket)
     db.session.commit()
-    return service_ticket_schema.jsonify(new_service_ticket), 201
+    return service_tickets_schema.jsonify(new_service_ticket), 201
 
 
 #Read S_T
-@service_tickes_bp.route('/service_tickets', methods=["GET"])
+@service_tickets_bp.route('/service_tickets', methods=["GET"])
 def read_service_tickets():
     service_tickets = db.session.query(Service_Tickets).all()
     return service_tickets_schema.jsonify(service_tickets), 200
@@ -31,13 +31,13 @@ def read_service_tickets():
 @service_tickets_bp.route('/service_tickets/<int:service_ticket_id>', methods=["GET"])
 def read_service_tickets():
     service_tickets = db.session.get(Service_Tickets, service_ticket_id)
-    return service_tickets_schema.jsonify(service_ticket), 200
+    return service_tickets_schema.jsonify(service_tickets), 200
 
 
 #Delete
 @service_tickets_bp.route('/service_tickets/<int:service_ticket_id>', methods=["DELETE"])
 def read_service_ticket(service_ticket_id):
-    service_ticket = db.session.get(Service_tickets, service_ticket_id)
+    service_ticket = db.session.get(Service_Tickets, service_ticket_id)
     db.session.delete(service_ticket)
     db.session.commit()
     return jsonify({"messgae": f"Successfully Deleted Service_Ticket {service_ticket_id}"}), 200
@@ -52,7 +52,7 @@ def update_service_ticket(service_ticket_id):
         return jsonify({"message": "service_ticket not found"}), 404 #if not return error message
     
     try:
-        service_ticket_data = service_ticket_schema.load(request.json) #Validating updates
+        service_ticket_data = service_tickets_schema.load(request.json) #Validating updates
     except ValidationError as e:
         return jsonify({"message": e.messages}), 400
     
@@ -60,4 +60,4 @@ def update_service_ticket(service_ticket_id):
         setattr(service_ticket, key, value) # setting Object, Attribute, Value to replace
 
     db.session.commit()
-    return service_ticket_schema.jsonify(service_ticket), 200
+    return service_tickets_schema.jsonify(service_ticket), 200
